78af31b77a2a1a615899af6d9087a7dba8065835
Nicolás Gabriel Gallegos <ngallegos@frro.utn.edu.ar>
2024-08-21 04:02:34 -0300

    Actualización del Modelo
    Modificación:
    - Columnas de la tabla Perfil

--- App/Servicios/Modelo/Perfil.cs
+++ App/Servicios/Modelo/Perfil.cs
@@ -1,15 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Servicios.Modelo
 {
     public class Perfil
     {
         [Key]
-        public int IdPerfil { get; set; }   // PK, Autogenerado, Incremental, Identidad
-        public int DNI { get; set; }        // FK a un usuario
-        public int TipoPerfil { get; set; } // Indica el tipo de perfil - 0: Alumno; 1: Docente; 2: Administrador
-        public int? Legajo { get; set; }    // Legajo del usuario, es null si el usuarios es de tipo "Administrador"
-        public bool Activado { get; set; }  // Indica si el perfil está habilitado por un administrador
+        public int IdPerfil { get; set; }           // PK, Autogenerado, Incremental, Identidad
+        public int DNI { get; set; }                // FK a un usuario
+        public int TipoPerfil { get; set; }         // Indica el tipo de perfil - 0: Alumno; 1: Docente; 2: Administrador
+        public int? Legajo { get; set; }            // Legajo del usuario, es null si el usuarios es de tipo "Administrador"
+        public int? IdEspecialidad { get; set; }    // Si es alumno, especialidad en la que cursa. Sino, null
+        public bool Activado { get; set; }          // Indica si el perfil está habilitado por un administrador
     }
 }
